ce182f062e82789fdc803e745da5bdc5ec2b279d
hugosousa42 <hugosousa42@proton.me>
2024-09-04 16:38:30 +0000

    InitProject

---
+++ Sales_NET8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sales_NET8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
